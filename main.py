8c74109d49459aa51af18fc0fdd77d1e84d68027
Basantyaser <basntyaser90@gmail.com>
2026-05-26 13:39:46 +0300

    app complete

---
+++ main.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+from tkinter import ttk
+import customtkinter as ctk
+import string
+import random
+import secrets
+import math
+
+
+class Passwordgenerator:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("350x250")
+        self.root.title("Password Generator")
+        self.root.resizable(False, False)
+        self.strength_var  = ctk.StringVar(value="18")
+        self.password_var  = ctk.StringVar()
+        # call a method to build GUI elements
+        self.create_widgets()
+    def create_widgets(self):
+        ctk.CTkLabel(self.root, text="Enter password length:", font=("Arial", 10)).pack(pady=10)
+        strength_value = ctk.CTkEntry(self.root, textvariable=self.strength_var)
+        strength_value.pack(pady=10)
+        generate_btn = ctk.CTkButton(self.root, text="Generate Password", command=self.generate)
+        generate_btn.pack(pady=10)
+        ctk.CTkEntry(self.root, textvariable=self.password_var, justify="center", width=230, font=("Arial", 12), state="readonly").pack(pady=10)
+        self.entropy_label = ctk.CTkLabel(self.root, text="", font=("Arial", 10))
+        self.entropy_label.pack(pady=10)
+
+    def generate(self):
+        strength = self.strength_var.get()
+
+        chars = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
+        password = "".join(secrets.choice(chars) for _ in range(int(strength)))
+
+
+        # calculate Entroy using the formula E = L * log2(N)
+        entropy = int(strength) * math.log2(len(chars))
+
+        if entropy < 28:
+            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Very Weak)", text_color="red")
+        elif 28 <= entropy < 36:
+            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Weak)", text_color="orange")
+        elif 36 <= entropy < 60:
+            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Medium)", text_color="yellow")
+        elif 60 <= entropy < 128:
+            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Strong)", text_color="green")
+        else:
+            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Very Strong)", text_color="blue")
+
+            # Display the password in the entry box
+            self.password_var.set(password)
+
+
+
+
+
+if __name__ == "__main__":
+    root = ctk.CTk()
+    app = Passwordgenerator(root)
+    root.mainloop()
