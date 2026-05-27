a28cdffd3d4e2beb2d7a3a10d23154476d1e04db
Basantyaser <basntyaser90@gmail.com>
2026-05-27 18:32:49 +0300

    readme file edited, GUI screenshot added

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# password_generator_app
+
+# Password Generator Application
 project_week3_decodeLabs
+
+A secure password generator desktop application built with Python, Tkinter, and CustomTkinter.
+
+This application allows users to generate strong random passwords based on a custom length entered by the user. The generated password is analyzed using entropy calculations to determine its overall strength.
+
+---
+
+# Features
+
+- Generate secure random passwords
+- User-defined password length
+- Password entropy calculation
+- Password strength analysis
+- Modern GUI using CustomTkinter
+- Cryptographically secure generation using the `secrets` module
+- Simple and user-friendly interface
+
+---
+
+# Screenshots
+
+## Main GUI
+
+
+![alt text](image.png)
+
+---
+
+# Technologies Used
+
+- Python
+- Tkinter
+- CustomTkinter
+- secrets module
+- string module
+- math module
+
+---
+
+# Installation
+
+## Clone the repository
+
+```bash
+git clone https://github.com/Basantyaser/password_generator_app.git
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import math
 class Passwordgenerator:
     def __init__(self, root):
         self.root = root
-        self.root.geometry("350x250")
+        self.root.geometry("550x550")
         self.root.title("Password Generator")
         self.root.resizable(False, False)
         self.strength_var  = ctk.StringVar(value="18")
@@ -38,15 +38,15 @@ class Passwordgenerator:
         entropy = int(strength) * math.log2(len(chars))
 
         if entropy < 28:
-            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Very Weak)", text_color="red")
+            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Very Weak)", text_color="red", font=("Arial", 22, "bold"))
         elif 28 <= entropy < 36:
-            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Weak)", text_color="orange")
+            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Weak)", text_color="orange", font=("Arial", 22, "bold"))
         elif 36 <= entropy < 60:
-            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Medium)", text_color="yellow")
+            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Medium)", text_color="yellow", font=("Arial", 22, "bold"))
         elif 60 <= entropy < 128:
-            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Strong)", text_color="green")
+            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Strong)", text_color="green", font=("Arial", 22, "bold"))
         else:
-            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Very Strong)", text_color="blue")
+            self.entropy_label.configure(text=f"Entropy: {entropy:.2f} bits (Very Strong)", text_color="blue", font=("Arial", 22, "bold"))
 
             # Display the password in the entry box
         self.password_var.set(password)
